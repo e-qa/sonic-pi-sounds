ace8e950dfac4a93040379b12d4308f21229e0b4
Ali <eliqarayev835@gmail.com>
2024-08-14 21:27:35 +0400

    initial commit

---
+++ crazy_frog_melody.rb
@@ -0,0 +1,59 @@
+use_synth :dsaw
+use_bpm 120
+
+live_loop :crazy_frog_melody do
+  play :D5
+  sleep 0.5
+  play :F5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :G5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :C5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :A5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :Bb5
+  sleep 0.5
+  play :A5
+  sleep 0.5
+  play :F5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :A5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :C5
+  sleep 0.5
+  play :C5
+  sleep 0.5
+  play :A5
+  sleep 0.5
+  play :E5
+  sleep 0.5
+  play :D5
+  sleep 1
+end
+
+
+live_loop :beat do
+  sample :bd_haus, amp: 2
+  sleep 1
+end
+
+
